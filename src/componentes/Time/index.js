5e4382fcdd3976af1fa690f7305f5eb617e3e237
Roberto Ferreira <robertoferreirajrr@gmail.com>
2023-04-20 16:07:48 -0300

    corrigindo

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -7,7 +7,7 @@ const Time = (props) => {
     return (
         //Para o JSX é tudo uma linha só! Para ele se deu false ele nem roda o da direita!
         //ou (props.colaboradores.length) ? '' : '' -> operador ternário
-        (props.colaboradores.length) && <section className='time' style={css}>
+        (props.colaboradores.length > 0) && <section className='time' style={css}>
         <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
         <div className='colaboradores'>
         {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} corDeFundo={props.corPrimaria} imagem={colaborador.imagem} key={colaborador.nome}/>)}
